7046eed14f24a9e99f8c4b2241dfb236a2696d2b
Yang Hao-Wei <w95wayne10@gmail.com>
2017-12-20 00:46:46 +0800

    add file

---
+++ mnist-svhn-transfer3/solver.py
@@ -0,0 +1,238 @@
+import torch
+import torch.nn as nn
+import torchvision
+import os
+import pickle
+import scipy.io
+import numpy as np
+
+from torch.autograd import Variable
+from torch import optim
+from model import G12, G21 , G32, G23
+from model import D3, D2
+
+
+class Solver(object):
+    def __init__(self, config, svhn_loader, mnist_loader):
+        self.svhn_loader = svhn_loader
+        self.mnist_loader = mnist_loader
+        self.g12 = None
+        self.g21 = None
+        self.g32 = None
+        self.g23 = None
+        self.d3 = None
+        self.d2 = None
+        self.g_optimizer = None
+        self.d_optimizer = None
+        self.use_reconst_loss = config.use_reconst_loss
+        self.use_labels = config.use_labels
+        self.num_classes = config.num_classes
+        self.beta1 = config.beta1
+        self.beta2 = config.beta2
+        self.g_conv_dim = config.g_conv_dim
+        self.d_conv_dim = config.d_conv_dim
+        self.train_iters = config.train_iters
+        self.batch_size = config.batch_size
+        self.lr = config.lr
+        self.log_step = config.log_step
+        self.sample_step = config.sample_step
+        self.sample_path = config.sample_path
+        self.model_path = config.model_path
+        self.build_model()
+        
+    def build_model(self):
+        """Builds a generator and a discriminator."""
+        self.g12 = G12(conv_dim=self.g_conv_dim)
+        self.g21 = G21(conv_dim=self.g_conv_dim)
+        self.g32 = G32(conv_dim=self.g_conv_dim)
+        self.g23 = G23(conv_dim=self.g_conv_dim)
+        self.d3 = D3(conv_dim=self.d_conv_dim, use_labels=self.use_labels)
+        self.d2 = D2(conv_dim=self.d_conv_dim, use_labels=self.use_labels)
+        
+        g_params = list(self.g12.parameters()) + list(self.g21.parameters()) + list(self.g32.parameters()) + list(self.g23.parameters())
+        d_params = list(self.d3.parameters()) + list(self.d2.parameters())
+        
+        self.g_optimizer = optim.Adam(g_params, self.lr, [self.beta1, self.beta2])
+        self.d_optimizer = optim.Adam(d_params, self.lr, [self.beta1, self.beta2])
+        
+        if torch.cuda.is_available():
+            self.g12.cuda()
+            self.g21.cuda()
+            self.g32.cuda()
+            self.g23.cuda()
+            self.d3.cuda()
+            self.d2.cuda()
+    
+    def merge_images(self, sources, targets, k=10):
+        _, _, h, w = sources.shape
+        row = int(np.sqrt(self.batch_size))
+        merged = np.zeros([3, row*h, row*w*2])
+        for idx, (s, t) in enumerate(zip(sources, targets)):
+            i = idx // row
+            j = idx % row
+            merged[:, i*h:(i+1)*h, (j*2)*h:(j*2+1)*h] = s
+            merged[:, i*h:(i+1)*h, (j*2+1)*h:(j*2+2)*h] = t
+        return merged.transpose(1, 2, 0)
+    
+    def to_var(self, x):
+        """Converts numpy to variable."""
+        if torch.cuda.is_available():
+            x = x.cuda()
+        return Variable(x)
+    
+    def to_data(self, x):
+        """Converts variable to numpy."""
+        if torch.cuda.is_available():
+            x = x.cpu()
+        return x.data.numpy()
+    
+    def reset_grad(self):
+        """Zeros the gradient buffers."""
+        self.g_optimizer.zero_grad()
+        self.d_optimizer.zero_grad()
+
+    def train(self):
+        svhn_iter = iter(self.svhn_loader)
+        mnist_iter = iter(self.mnist_loader)
+        iter_per_epoch = min(len(svhn_iter), len(mnist_iter))
+        
+        # fixed mnist and svhn for sampling
+        fixed_svhn = self.to_var(svhn_iter.next()[0])
+        fixed_mnist = self.to_var(mnist_iter.next()[0])
+        
+        # loss if use_labels = True
+        criterion = nn.CrossEntropyLoss()
+        
+        for step in range(self.train_iters+1):
+            # reset data_iter for each epoch
+            if (step+1) % iter_per_epoch == 0:
+                mnist_iter = iter(self.mnist_loader)
+                svhn_iter = iter(self.svhn_loader)
+            
+            # load svhn and mnist dataset
+            svhn, s_labels = svhn_iter.next() 
+            svhn, s_labels = self.to_var(svhn), self.to_var(s_labels).long().squeeze()
+            mnist, m_labels = mnist_iter.next() 
+            mnist, m_labels = self.to_var(mnist), self.to_var(m_labels)
+
+            if self.use_labels:
+                mnist_fake_labels = self.to_var(
+                    torch.Tensor([self.num_classes]*svhn.size(0)).long())
+                svhn_fake_labels = self.to_var(
+                    torch.Tensor([self.num_classes]*mnist.size(0)).long())
+            
+            #============ train D ============#
+            
+            # train with real images
+            self.reset_grad()
+            out = self.d3(-mnist)
+            if self.use_labels:
+                d3_loss = criterion(out, m_labels)
+            else:
+                d3_loss = torch.mean((out-1)**2)
+            
+            out = self.d2(svhn)
+            if self.use_labels:
+                d2_loss = criterion(out, s_labels)
+            else:
+                d2_loss = torch.mean((out-1)**2)
+            
+            d_mnist_loss = d3_loss
+            d_svhn_loss = d2_loss
+            d_real_loss = d3_loss + d2_loss
+            d_real_loss.backward()
+            self.d_optimizer.step()
+            
+            # train with fake images
+            self.reset_grad()
+            fake_svhn = self.g12(mnist)
+            out = self.d2(fake_svhn)
+            if self.use_labels:
+                d2_loss = criterion(out, svhn_fake_labels)
+            else:
+                d2_loss = torch.mean(out**2)
+            
+            fake_mnist = self.g23(svhn)
+            out = self.d3(fake_mnist)
+            if self.use_labels:
+                d3_loss = criterion(out, mnist_fake_labels)
+            else:
+                d3_loss = torch.mean(out**2)
+            
+            d_fake_loss = d3_loss + d2_loss
+            d_fake_loss.backward()
+            self.d_optimizer.step()
+            
+            #============ train G ============#
+            
+            # train mnist-svhn-mnist cycle
+            self.reset_grad()
+            fake_svhn = self.g12(mnist)
+            out = self.d2(fake_svhn)
+            reconst_mnist = self.g21(fake_svhn)
+            if self.use_labels:
+                g_loss = criterion(out, m_labels) 
+            else:
+                g_loss = torch.mean((out-1)**2) 
+
+            if self.use_reconst_loss:
+                g_loss += torch.mean((mnist - reconst_mnist)**2)
+
+            g_loss.backward()
+            self.g_optimizer.step()
+
+            # train svhn-mnist-svhn cycle
+            self.reset_grad()
+            fake_mnist = self.g23(svhn)
+            out = self.d3(fake_mnist)
+            reconst_svhn = self.g32(fake_mnist)
+            if self.use_labels:
+                g_loss = criterion(out, s_labels) 
+            else:
+                g_loss = torch.mean((out-1)**2) 
+
+            if self.use_reconst_loss:
+                g_loss += torch.mean((svhn - reconst_svhn)**2)
+
+            g_loss.backward()
+            self.g_optimizer.step()
+            
+            # print the log info
+            if (step+1) % self.log_step == 0:
+                print('Step [%d/%d], d_real_loss: %.4f, d_mnist_loss: %.4f, d_svhn_loss: %.4f, '
+                      'd_fake_loss: %.4f, g_loss: %.4f' 
+                      %(step+1, self.train_iters, d_real_loss.data[0], d_mnist_loss.data[0], 
+                        d_svhn_loss.data[0], d_fake_loss.data[0], g_loss.data[0]))
+
+            # save the sampled images
+            if (step+1) % self.sample_step == 0:
+                fake_svhn = self.g12(fixed_mnist)
+                fake_mnist = self.g23(fixed_svhn)
+                
+                mnist, fake_mnist = self.to_data(fixed_mnist), self.to_data(fake_mnist)
+                svhn , fake_svhn = self.to_data(fixed_svhn), self.to_data(fake_svhn)
+                
+                merged = self.merge_images(mnist, fake_svhn)
+                path = os.path.join(self.sample_path, 'sample-%d-m-s.png' %(step+1))
+                scipy.misc.imsave(path, merged)
+                print ('saved %s' %path)
+                
+                merged = self.merge_images(svhn, fake_mnist)
+                path = os.path.join(self.sample_path, 'sample-%d-s-m.png' %(step+1))
+                scipy.misc.imsave(path, merged)
+                print ('saved %s' %path)
+            
+            if (step+1) % 5000 == 0:
+                # save the model parameters for each epoch
+                g12_path = os.path.join(self.model_path, 'g12-%d.pkl' %(step+1))
+                g21_path = os.path.join(self.model_path, 'g21-%d.pkl' %(step+1))
+                g32_path = os.path.join(self.model_path, 'g32-%d.pkl' %(step+1))
+                g23_path = os.path.join(self.model_path, 'g23-%d.pkl' %(step+1))
+                d3_path = os.path.join(self.model_path, 'd3-%d.pkl' %(step+1))
+                d2_path = os.path.join(self.model_path, 'd2-%d.pkl' %(step+1))
+                torch.save(self.g12.state_dict(), g12_path)
+                torch.save(self.g21.state_dict(), g21_path)
+                torch.save(self.g32.state_dict(), g32_path)
+                torch.save(self.g23.state_dict(), g23_path)
+                torch.save(self.d3.state_dict(), d3_path)
+                torch.save(self.d2.state_dict(), d2_path)
--- report.md
+++ report.md
@@ -1,17 +1,45 @@
 # Homework4 report
 
-### What scenario do I apply in?
-you are encouraged to elaborate the motivation here
+### What scenario do I apply in?  
+利用 mnist 和負片 mnist 去處理 svhn的截圖數字資料  
+在 mnist的高判斷比率狀況下 了解機器對 svhn的理解情形  
 
-### What do I modify? 
-you can show some snippet
+### What do I modify?   
+1、2資料集為原作者的處理情況
+A->B*->A 和 B->A*->B*流程   
+2是讓mnist負片後代入原訓練過程的結果  
+3是原先照著題目講到的  
+A->B*->A 和 B->C*->B*流程  
+但後續發現對B的擴展仍然只有如1、2的效果  
+故補齊了  
+B->A*->B 和 C->B*->C*流程  
+形成最後的結果
 
 ### Qualitative results
-put some interesting images generated from your Bi-CycleGANs
+model 1:  
+![md1]()
+Step [20000/20000], d_real_loss: 0.0447, d_mnist_loss: 0.0035, d_svhn_loss: 0.0412, d_fake_loss: 0.0404, g_loss: 1.1687  
+
+model 2:  
+![md2]()  
+Step [20000/20000], d_real_loss: 0.0422, d_mnist_loss: 0.0020, d_svhn_loss: 0.0402, d_fake_loss: 0.0403, g_loss: 1.1395  
+
+model 3:  
+![md3]()
+Step [20000/20000], d_real_loss: 0.0863, d_mnist_loss: 0.0056, d_svhn_loss: 0.0807, d_fake_loss: 0.2429, g_loss: 0.9839  
+
+model 4:  
+![md4_1]() ![md4_2]()    
+Step [10000/10000], d_real_loss: 0.1051, d_mnist_loss: 0.0320, d_svhn_loss: 0.0731, d_fake_loss: 0.0835, g_loss: 1.1164  
 
 ### My thoughts 
-you can make some comments on the your own homework, e.g. what's the strength? what's the limitation?
+在背景相似度高的時候生成mnist會有良好的效果
+而生成svhn的時候由於mnist資料的單調情形 會生成比較單一的結果
+後續在確定如何鎖定label之後或許可生成除了像數字之外 包含正確性的結果
 
 ### Others
+純屬意外的狀況  
+![mdout]()
 
 ### Reference
+[原作者](https://github.com/yunjey/mnist-svhn-transfer)  
